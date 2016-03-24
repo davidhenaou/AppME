3903a6088c7445252b45194fe2cccc420f92abdc
davidhenaou <davidhenaou@gmail.com>
2016-03-23 23:11:11 -0500

    se agrego authenticacion simple y compleja

--- app/src/main/java/com/dhenao/miestadio/ActividadPrincipal.java
+++ app/src/main/java/com/dhenao/miestadio/ActividadPrincipal.java
@@ -1,5 +1,8 @@
 package com.dhenao.miestadio;
 
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
@@ -193,6 +196,29 @@ public class ActividadPrincipal extends AppCompatActivity {
                 break;
             case R.id.nav_2:
                 fragmentoGenerico = CargarContenidoViewPager.nuevaInstancia(2, 4);
+                break;
+            case R.id.nav_3:
+                String username = "david";
+                String password = "324126";
+
+                Account account = new Account(username, getString(R.string.ACCOUNT_TYPE));
+                AccountManager am = AccountManager.get(this);
+                boolean accountCreated = am.addAccountExplicitly(account, password, null);
+
+                Bundle extras = getIntent().getExtras();
+                if (extras != null) {
+                    if (accountCreated) {  //Pass the new account back to the account manager
+                        AccountAuthenticatorResponse response = extras.getParcelable(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE);
+                        Bundle result = new Bundle();
+                        result.putString(AccountManager.KEY_ACCOUNT_NAME, username);
+                        result.putString(AccountManager.KEY_ACCOUNT_TYPE, getString(R.string.ACCOUNT_TYPE));
+                        response.onResult(result);
+                    }
+                    finish();
+                }
+
+
+
                 break;
             case R.id.nav_5:
                 new CargarLosEquipos().execute();
